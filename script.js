5a852ec6f8fbc2db5b52e251b9f8b73c9ab16939
Chandu_Pingili <126405951+ChanduPingili@users.noreply.github.com>
2023-11-13 23:28:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
-
-function add(){
-  let a = document.getElementById("value1").value*1
-  let b = document.getElementById("value2").value*1
-  document.getElementById("result").innerHTML=a+b;
-}
-function sub(){
-  let a = document.getElementById("value1").value*1
-  let b = document.getElementById("value2").value*1
-  document.getElementById("result").innerHTML=a-b;
-}
-function mul(){
-  let a = document.getElementById("value1").value*1
-  let b = document.getElementById("value2").value*1
-  document.getElementById("result").innerHTML=a*b;
-}
-function div(){
-  let a = document.getElementById("value1").value*1
-  let b = document.getElementById("value2").value*1
-  document.getElementById("result").innerHTML=a/b;
+window.onload=function(){
+  let str = "";
+  let scr = document.getElementsByClassName("input")[0];
+  let on="off";
+  [...document.getElementsByClassName("btn")].forEach(function(item){
+    item.addEventListener("click",function(){
+      let val = this.value;
+      if(val === "on"){
+        document.getElementById("screen").style.background= "rgb(151, 224, 224)";
+        on = val;
+      }
+      else if(val==="off"){
+        document.getElementById("screen").style.background= "rgb(217, 251, 252)";
+        on = val;
+        scr.innerHTML = "";
+      }
+      else if(on=== "on"){
+        if(val==="cls"){
+          str = "";
+          scr.innerHTML = "";
+        }
+        else if(val ==="="){
+          str = eval(str);
+          str+='';
+          scr.innerHTML = str;
+        }
+        else if(val=="<-"){
+          str+='';
+          str = str.substring(0,str.length-1)
+          if(str.length == 0)
+            scr.innerHTML = "";
+          else
+            scr.innerHTML = str;
+        }
+        else{
+          let value = this.value;
+          str += value;
+          scr.innerHTML += value;
+        }
+      }
+    })
+  })
 }
